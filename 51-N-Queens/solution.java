f350fc9d0a328e0911b6a71032c80ac3f5a40127
Ben Y <byue2013@my.fit.edu>
2016-08-28 17:46:24 -0400

    [51. N-Queens][Accepted]

---
+++ 51-N-Queens/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res=new ArrayList<List<String>>();
+        String[] queens=new String[n];
+        char[] initial=new char[n];
+        Arrays.fill(initial,'.');
+        Arrays.fill(queens,String.valueOf(Arrays.copyOf(initial,n)));
+        int[] flag=new int[5*n-2];
+        Arrays.fill(flag,1);
+        slove(res,queens,flag,0,n);
+        return res;
+    }
+    private void slove(List<List<String>> res,String[] queens,int[] flag,int row,int n){
+        if(row==n){
+            res.add(new ArrayList<String>(Arrays.asList(queens)));
+            return;
+        }
+        for(int col=0;col!=n;col++){
+            if(flag[col]==1 && flag[n+col+row]==1 && flag[4*n-2+col-row]==1){
+                flag[col]=0;
+                flag[n+col+row]=0;
+                flag[4*n-2+col-row]=0;
+                char[] chars=queens[row].toCharArray();
+                
+                chars[col]='Q';
+                queens[row]=String.valueOf(chars);
+                
+                slove(res,queens,flag,row+1,n);
+                
+                chars=queens[row].toCharArray();
+                chars[col]='.';
+                queens[row]=String.valueOf(chars);
+                
+                flag[col]=1;
+                flag[n+col+row]=1;
+                flag[4*n-2+col-row]=1;
+            }
+        }
+    }
+}
