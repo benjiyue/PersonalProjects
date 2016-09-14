5b168057aafa4475ab7cc62d441d6403c7b012db
Ben Y <byue2013@my.fit.edu>
2016-09-13 20:35:27 -0400

    [45. Jump Game II][Accepted]

--- 45-Jump-Game-II/solution.java
+++ 45-Jump-Game-II/solution.java
@@ -1,20 +1,23 @@
 public class Solution {
-	 public int jump(int[] A) {
-		 if(A==null||A.length<2)
-			 return 0;
-		 int iter = 0;
-		 int nextLevelMax = 0;
-		 int currentLevelMax = 0;
-		 int steps = 1;
-		 while(currentLevelMax-iter>=0){
-			 for(;iter<=currentLevelMax;iter++){
-				 nextLevelMax = Math.max(nextLevelMax, iter+A[iter]);
-				 if(nextLevelMax>=A.length-1)
-					 return steps;
-			 }
-			 steps++;
-			 currentLevelMax = nextLevelMax;
-		 }
-		 return steps;
-	 }
+    public int jump(int[] nums) {
+        if(nums.length<2)
+            return 0;
+        //idea is to jump as for as possible for your range thus far, so you always have to take the first jump then anchor the right position, the iterate for each position in the range and see where is the farthest i can get from things in this range and then iterate over things in the range from my end to the new longest range
+        
+        //so idea is that each time i have two anchors for where i can be.
+        //inclusive
+        int lowRange = 1;
+        int highRange = nums[0];
+        int steps = 1;
+        while(highRange<nums.length-1){
+            int newMax = 0;
+            for(int i=lowRange;i<=highRange;i++){
+                newMax = Math.max(newMax, nums[i]+i);
+            }
+            lowRange = highRange+1;
+            highRange = newMax;
+            steps++;
+        }
+        return steps;
+    }
 }
