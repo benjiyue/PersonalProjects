e97b4d2b025be63917ef2980a05bbb3ee84721a7
Ben Y <byue2013@my.fit.edu>
2016-09-14 18:50:20 -0400

    [42. Trapping Rain Water][Accepted]

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -1,24 +1,26 @@
 public class Solution {
     public int trap(int[] height) {
-        //the idea is that we iterate left to right on the array along with a stack, if the current index is less than or equal to the previous height then insert it into the stack ,otherwise the current thing is the right wall and we poll from the stack. The height is the min of the left or right of the popped and the distance is the width
-        
-        if(height==null||height.length<3)
+
+        if(height.length<3)
             return 0;
-        int totalArea = 0;
+        int area = 0;
         Stack<Integer> stack = new Stack();
-        int current = 0;
-        //why less than or equal to?
-        while(current<height.length){
-            if(stack.isEmpty()||height[current]<=height[stack.peek()])
-                stack.push(current++);
+        int counter = 0;
+        while(counter<height.length){
+            if(stack.isEmpty()||height[counter]<height[stack.peek()])
+                stack.push(counter++);
+            //stack is greater than or equal to peek
             else{
                 int poppedIndex = stack.pop();
-                int area = stack.isEmpty()?0:
-                (Math.min(height[current], height[stack.peek()])-height[poppedIndex])*(current-stack.peek()-1);
-                totalArea += area;
+                int floorHeight = height[poppedIndex];
+                if(stack.isEmpty())
+                    continue;
+                int leftWallHeight = height[stack.peek()];
+                int min = Math.min(height[counter], leftWallHeight);
+                int diff = min-floorHeight;
+                area+=diff*(counter-stack.peek()-1);
             }
         }
-        //why no need to pop off remainder?
-        return totalArea;
+        return area;
     }
 }
