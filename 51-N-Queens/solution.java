a7b7b471718e0006af4c96a0389a986f2fa504b7
Ben Y <byue2013@my.fit.edu>
2016-09-16 17:49:34 -0400

    [51. N-Queens][Accepted]

--- 51-N-Queens/solution.java
+++ 51-N-Queens/solution.java
@@ -1,40 +1,55 @@
 public class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res=new ArrayList<List<String>>();
-        String[] queens=new String[n];
-        char[] initial=new char[n];
-        Arrays.fill(initial,'.');
-        Arrays.fill(queens,String.valueOf(Arrays.copyOf(initial,n)));
-        int[] flag=new int[5*n-2];
-        Arrays.fill(flag,1);
-        slove(res,queens,flag,0,n);
+        /*
+        iterate on the row, have column boolean array
+        have upRight has index col-row , and upLeft 2*length boolean array with index sum of the two
+        */
+        res = new LinkedList();
+        this.n = n;
+        board = new boolean[n][n];
+        cols = new boolean[n];
+        //diff the two
+        upLeft = new boolean[2*n];
+        //sum the two
+        upRight = new boolean[2*n];
+        dfs(0, 0);
         return res;
     }
-    private void slove(List<List<String>> res,String[] queens,int[] flag,int row,int n){
-        if(row==n){
-            res.add(new ArrayList<String>(Arrays.asList(queens)));
+    List<List<String>> res;
+    int n;
+    boolean[][] board;
+    boolean[] cols;
+    boolean[] upLeft;
+    boolean[] upRight;
+    
+    public void dfs(int row, int counter){
+        if(counter==n){
+            addString();
             return;
         }
-        for(int col=0;col!=n;col++){
-            if(flag[col]==1 && flag[n+col+row]==1 && flag[4*n-2+col-row]==1){
-                flag[col]=0;
-                flag[n+col+row]=0;
-                flag[4*n-2+col-row]=0;
-                char[] chars=queens[row].toCharArray();
-                
-                chars[col]='Q';
-                queens[row]=String.valueOf(chars);
-                
-                slove(res,queens,flag,row+1,n);
-                
-                chars=queens[row].toCharArray();
-                chars[col]='.';
-                queens[row]=String.valueOf(chars);
-                
-                flag[col]=1;
-                flag[n+col+row]=1;
-                flag[4*n-2+col-row]=1;
+        for(int a = 0;a<n;a++){
+            if(!cols[a]&&!upLeft[a-row+n-1]&&!upRight[row+a]){
+                cols[a]=upLeft[a-row+n-1]=upRight[row+a] = true;
+                board[row][a] = true;
+                dfs(row+1, counter+1);
+                board[row][a] = false;
+                cols[a]=upLeft[a-row+n-1]=upRight[row+a] = false;
             }
         }
+    }
+    
+    public void addString(){
+        List<String> thisRes = new LinkedList();
+        for(int i=0;i<n;i++){
+            StringBuilder cur = new StringBuilder();
+            for(int j=0;j<n;j++){
+                if(board[i][j]){
+                    cur.append('Q');
+                }else
+                    cur.append('.');
+            }
+            thisRes.add(cur.toString());
+        }
+        res.add(thisRes);
     }
 }
