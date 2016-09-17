3cd73a6685d9e0bacbdd467dd2effecdd10f2fd9
Ben Y <byue2013@my.fit.edu>
2016-09-17 00:04:44 -0400

    [87. Scramble String][Accepted]

--- 87-Scramble-String/solution.java
+++ 87-Scramble-String/solution.java
@@ -1,54 +1,52 @@
 public class Solution {
-	boolean isScramble(String _a, String _b){
-		a = _a;
-		b = _b;
-		map = new HashMap<String, Boolean>();
-		return isScramble(0, a.length(), 0, b.length());
+	public boolean isScramble(String s1, String s2) {
+		//the solution is recursive and tries dividing the scrammbled word range into 2 parts such that it's from or back matches the same sized front of the orig, if it matches either then recurse for that, ie they're anagrams by iterating over their counts if so recurse
+		map = new HashMap();
+		return search(s1, s2);
+
 	}
-	
 	Map<String, Boolean> map;
-	String a;
-	String b;
 	
-	boolean isScramble(int aLow, int aHigh, int bLow, int bHigh) {
-		String key = aLow+","+aHigh+","+bLow+","+bHigh;
-		if(a.substring(aLow, aHigh).equals(b.substring(bLow, bHigh))){
-			map.put(key, true);
+	//returns whether the given range can be rotations
+	public boolean search(String orig, String scrambled){
+		if(scrambled.equals(orig))
 			return true;
+		String key = orig+","+scrambled;
+		if(map.containsKey(key))
+			return map.get(key);
+		//checkCount
+		if(!sameCounts(orig, scrambled)){
+			map.put(key, false);
+			return false;
 		}
-		
-		int[] aCount = new int[26];
-		int[] bCount = new int[26];
-		for(int i=0;i<(aHigh-aLow);i++){
-			aCount[a.charAt(aLow+i)-'a']++;
-			bCount[b.charAt(bLow+i)-'a']++;
+		boolean res = false;
+		for(int i=1;i<=orig.length()-1;i++){
+			//
+			String front = orig.substring(0,i);
+			String back = orig.substring(i);
+			boolean frontTofront = search(front,scrambled.substring(0,i))&&search(orig.substring(i),scrambled.substring(i));
+
+			boolean frontToBack = search(front,scrambled.substring(scrambled.length()-i))&&search(orig.substring(i),scrambled.substring(0, scrambled.length()-i));
+
+			if(frontTofront||frontToBack){
+				res = true;
+				break;
+			}
+		}
+		map.put(key, res);
+		return res;
+	}
+
+	public boolean sameCounts(String a, String b){
+		int[] counts = new int[26];
+		for(int i=0;i<a.length();i++){
+			counts[a.charAt(i)-'a']++;
+			counts[b.charAt(i)-'a']--;
 		}
 		for(int i=0;i<26;i++){
-			if(aCount[i]!=bCount[i]){
-				map.put(key, false);
+			if(counts[i]!=0)
 				return false;
-			}
 		}
-		
-		//i is the length of the left substring
-		for(int i=1;i<(aHigh-aLow);i++){
-			//first do them without inversion
-			//first chunks match exactly
-			if(isScramble(aLow, aLow+i, bLow, bLow+i)&&
-					isScramble(aLow+i, aHigh, bLow+i, bHigh)){
-				map.put(key, true);
-				return true;
-			}
-			if(isScramble(aLow, aLow+i,bHigh-i,bHigh)&&
-					isScramble(aLow+i, aHigh, bLow, bHigh-i)){
-				map.put(key, true);
-				return true;
-			}
-			//do them with inversion
-			//first match with back and back of second match with front
-		}
-		map.put(key, false);
-		return false;
-		
+		return true;
 	}
 }
