e0ad261eb6b62bffdb6e42450a6b8e188191fb36
Ben Y <byue2013@my.fit.edu>
2016-09-22 23:43:37 -0400

    [330. Patching Array][Accepted]

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,18 +1,16 @@
 public class Solution {
-	//given an array of sorted number give any patches needed such that any combination sum of numbers
-	//can add up to n
-	int minPatches(int[] nums, int n) {
-		long nextMaybeNot = 1;
-		int added = 0;
-		int iterator = 0;
-		while(nextMaybeNot<=n){
-			if(iterator<nums.length&&nums[iterator]<=nextMaybeNot){
-				nextMaybeNot+=nums[iterator++];
-			}else{
-				nextMaybeNot+=nextMaybeNot;
-				added++;
-			}
-		}
-		return added;
-	}
+    public int minPatches(int[] nums, int n) {
+        long nextPotentiallyMissing = 1;
+        int iter = 0;
+        int added = 0;
+        while(nextPotentiallyMissing<=n){
+            if(iter<nums.length&&nums[iter]<=nextPotentiallyMissing)
+                nextPotentiallyMissing+=nums[iter++];
+            else{
+                nextPotentiallyMissing+=nextPotentiallyMissing;
+                added++;
+            }
+        }
+        return added;
+    }
 }
