7085d87823c6d50cb7ba3dcbef37cad87abde6ac
Ben Y <byue2013@my.fit.edu>
2016-09-23 09:53:41 -0400

    [329. Longest Increasing Path in a Matrix][Accepted]

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -1,33 +1,37 @@
 public class Solution {
-	public int longestIncreasingPath(int[][] matrix) {
-		if(matrix.length==0||matrix[0].length==0)
-			return 0;
-		int[][] dp =  new int[matrix.length][matrix[0].length];
-		maxEver = 1;
-		for(int i=0;i<dp.length;i++){
-			for(int j=0;j<matrix[0].length;j++){
-				if(dp[i][j]==0)
-					dfs(matrix, dp, i, j);
-			}
-		}
-		return maxEver;
-	}
-	int maxEver;
-	int[][] directions = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
-	public int dfs(int[][] matrix, int[][] dp, int i, int j){
-		if(i<0||i>=matrix.length||j<0||j>=matrix[0].length)
-			return 0;
-		if(dp[i][j]!=0)
-			return dp[i][j];
-		int max = 1;
-		for(int a = 0;a<directions.length;a++){
-			int row = i+directions[a][0];
-			int col = j+directions[a][1];
-			if(row>=0&&row<matrix.length&&col>=0&&col<matrix[0].length&&matrix[row][col]>matrix[i][j])
-				max = Math.max(dfs(matrix, dp, row, col)+1, max);
-		}
-		dp[i][j] = max;
-		maxEver = Math.max(maxEver, max);
-		return max;
-	}
+    int[][] dp;
+    int[][] directions;
+    int maxEver;
+    public int longestIncreasingPath(int[][] matrix) {
+        //it's a dfs with memoization on places we've visited before. dp[i][j] is the longest we can go assuming cnchoring here
+        if(matrix.length==0||matrix[0].length==0)
+            return 0;
+        maxEver = 0;
+        directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        dp = new int[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                dfs(i,j, matrix);
+            }
+        }
+        return maxEver;
+    }
+    
+    public int dfs(int row, int col, int[][] matrix){
+        
+        if(dp[row][col]>0)
+            return dp[row][col];
+        
+        int maxDistance = 1;
+        for(int i=0;i<directions.length;i++){
+            int nextRow = row+directions[i][0];
+            int nextCol = col+directions[i][1];
+            if(nextRow>=0&&nextRow<dp.length&&nextCol>=0&&nextCol<dp[0].length&&matrix[nextRow][nextCol]>matrix[row][col]){
+                maxDistance = Math.max(maxDistance, dfs(nextRow, nextCol, matrix)+1);
+            }
+        }
+        dp[row][col] = maxDistance;
+        maxEver = Math.max(maxEver, maxDistance);
+        return maxDistance;
+    }
 }
