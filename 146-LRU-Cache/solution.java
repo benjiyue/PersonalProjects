64665d4c7872750d6ff4fc9b32f0ce398ad55d6b
Ben Y <byue2013@my.fit.edu>
2016-09-24 14:29:22 -0400

    [146. LRU Cache][Accepted]

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -1,76 +1,83 @@
-import java.util.*;
 public class LRUCache {
+    /*
+    Idea is to map values to node objects which are connecred via a linkedlist. with the front being oldest and end/right being newest thing and whenever we set an object, we check 
+    */
     
-    //have a linkedlist of nodes and a hashmap yo access the doubly linkedlist. And always invalidate from the front/head. Head and tail are dummy pointers. Get simply retrieves the item and puts it to the end of the array, set checks to see whether the key is in the list if so reset the val and put to end else create new and  put to end, but if size is too big, remove from the head
-    class Node{
-        public Node left;
-        public Node right;
-        public int val;
-        public int key;
+    class ListNode{
+        int key;
+        int val;
+        ListNode prev;
+        ListNode next;
+        public ListNode(int a, int b){
+            key = a;
+            val = b;
+        }
     }
     
-    Node head;
-    Node tail;
-    int capacity;
-    Map<Integer, Node> map;
-    
+    ListNode begin;
+    ListNode end;
+    int size;
+    Map<Integer, ListNode> map;
     public LRUCache(int capacity) {
-        head = new Node();
-        tail = new Node();
-        head.right = tail;
-        tail.left = head;
-        this.capacity = capacity;
+        begin = new ListNode(0,0);
+        end = new ListNode(0,0);
+        begin.next = end;
+        end.prev = begin;
+        size = capacity;
         map = new HashMap();
     }
     
     public int get(int key) {
-        if(map.containsKey(key)){
-            Node temp = map.get(key);
-            removeNode(temp.left, temp.right);
-            putToEnd(temp);
-            return temp.val;
-        }else
-            return -1;
-    }
-    
-    public void set(int key, int value) {
-        Node newNode;
-        if(map.containsKey(key)){
-            newNode = map.get(key);
-            newNode.val = value;
-            removeNode(newNode.left, newNode.right);
-            putToEnd(newNode);
-        }else{
-            newNode = new Node();
-            newNode.val = value;
-            newNode.key = key;
-            map.put(key, newNode);
-            
-            if(capacity==0){
-                Node toRemove = head.right;
-                map.remove(toRemove.key);
-                removeNode(head, head.right.right);
-                capacity++;
-            }
-            putToEnd(newNode);
-            capacity--;
+        ListNode gotten = map.get(key);
+        if(gotten!=null){
+            remove(gotten.prev, gotten.next);
+            putToBack(gotten);
         }
-            
+        return gotten==null?-1:gotten.val;
     }
     
-    //remove the node between left nd right
-    public void removeNode(Node left, Node right){
-        Node toRemove = left.right;
-        left.right = right;
-        right.left = left;
-        return;
+    /*
+    check to see if the key is already in the map if it is its simply giving it a new value
+    else it's new so create a new node. if over the size then remove the frontmost node from the map ie front.next; and also from the list
+    
+    In with case move it to the end afterwards.
+    */
+    public void set(int key, int value) {
+        ListNode gotten = map.get(key);
+        //new insertion maybe need to resize
+        
+        if(gotten==null){
+            ListNode newNode = new ListNode(key, value);
+            map.put(key, newNode);
+            if(size==0){
+                ListNode oldest = begin.next;
+                map.remove(oldest.key);
+                remove(begin, oldest.next);
+                size++;
+            }
+            putToBack(newNode);
+            size--;
+        }
+        //already exists
+        else{
+            gotten.val = value;
+            remove(gotten.prev, gotten.next);
+            putToBack(gotten);
+        }
     }
     
-    public void putToEnd(Node n){
-        Node before = tail.left;
-        before.right = n;
-        n.right = tail;
-        tail.left = n;
-        n.left = before;
+    //remove node given access to its neighbors
+    public void remove(ListNode before, ListNode after){
+        before.next = after;
+        after.prev = before;
+    }
+    
+    //insert to the back
+    public void putToBack(ListNode in){
+        ListNode beforeLast = end.prev;
+        beforeLast.next = in;
+        in.next = end;
+        in.prev = beforeLast;
+        end.prev = in;
     }
 }
