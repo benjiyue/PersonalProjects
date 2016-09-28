8556ca98f6d742aa0bc975ac123359659eaad0dd
Ben Y <byue2013@my.fit.edu>
2016-09-28 12:54:13 -0400

    [53. Maximum Subarray][Accepted]

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,25 +1,25 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
-        /*
-        we keep track of two variables: maxever and maxTHus far including the current element. if ever the maxThusFar goes below the current element we reset the maxThusFar to the current element
-        maxEver =    -2;
-        maxTHusFar = -2
-             i
-        [-2,-1,-9]
-        
+        /*Kaden's algo: start from beginning. maxEndHere, maxSoFar
+        As go, if this individual number beats accumulated sum, then restart maxEndHere
+        otherwise, continue maxEndHere
+        At any point if beat max, repalce it
         */
-        
         if(nums==null||nums.length==0)
             return 0;
+            
+        int maxSoFar = Integer.MIN_VALUE;
+        int maxEndHere = 0;
         
-        int maxEver = nums[0];
-        int maxSoFar = nums[0];
-        for(int i=1;i<nums.length;i++){
-            maxSoFar+=nums[i];
-            if(maxSoFar<nums[i])
-                maxSoFar = nums[i];
-            maxEver = Math.max(maxEver, maxSoFar);
+        for(int i=0;i<nums.length;i++){
+            maxEndHere+=nums[i];
+            if(nums[i]>maxEndHere)
+                maxEndHere = nums[i];
+            maxSoFar = Integer.max(maxSoFar, maxEndHere);
+            
         }
-        return maxEver;
+        
+        return maxSoFar;
+        
     }
 }
