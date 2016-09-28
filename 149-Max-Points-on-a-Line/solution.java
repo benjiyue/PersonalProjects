46f19d98e5f67265371c134e60e3ede3ca23b9d6
Ben Y <byue2013@my.fit.edu>
2016-09-28 11:48:23 -0400

    [149. Max Points on a Line][Accepted]

--- 149-Max-Points-on-a-Line/solution.java
+++ 149-Max-Points-on-a-Line/solution.java
@@ -9,33 +9,45 @@
  */
 public class Solution {
     public int maxPoints(Point[] points) {
+        /*
+        idea is that for each point we must iterate over all the other points and get the slope, then map this into a hasmap between slope and count of points on that slope
+        
+        we must also keep track of the same point as the anchor point and add this to all lines
+        
+        also points with 0 divisor need to be mapped to null
+        
+        */
+        
         if(points==null||points.length==0)
             return 0;
-        //idea is to iterate over all the points and use each as an anchor to find the slope between it and all others, if the slope is alrady in the map increment count else map it, the max will be the largest value in the set+1, then start with next;
+            
         int maxEver = 1;
         
         for(int i=0;i<points.length;i++){
-            //number of dups of i
-            int dups = 0;
-            int thisLevelMax = 0;
+            int thisMaxEver = 1;
             Map<Double, Integer> map = new HashMap();
+            int dups = 0;
             for(int j=0;j<points.length;j++){
-                if(j==i)    continue;
-                if(points[j].y==points[i].y&&points[j].x==points[i].x){
+                if(i==j)
+                    continue;
+                if(points[i].x==points[j].x&&points[i].y==points[j].y){
                     dups++;
                     continue;
                 }
-                Double slope = points[j].x!=points[i].x?
-                    (double)(points[j].y-points[i].y)/(double)(points[j].x-points[i].x):
-                    null;
-                if(!map.containsKey(slope))
-                    map.put(slope, 0);
-                int next = map.get(slope)+1;
-                map.put(slope, next);
-                thisLevelMax = Math.max(thisLevelMax, next);
+                Double slope = (points[i].x-points[j].x==0)?null:(double)(points[i].y-points[j].y)/(double)(points[i].x-points[j].x);
+                if(!map.containsKey(slope)){
+                    map.put(slope, 2);
+                    thisMaxEver = Math.max(2, thisMaxEver);
+                }
+                else{
+                    int newVal = map.get(slope)+1;
+                    map.put(slope, newVal);
+                    thisMaxEver = Math.max(newVal, thisMaxEver);
+                }
             }
-            maxEver = Math.max(thisLevelMax+1+dups, maxEver);
+            maxEver = Math.max(maxEver, thisMaxEver+dups);
         }
+        
         return maxEver;
     }
 }
