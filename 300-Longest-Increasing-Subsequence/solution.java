7b6c12195da9c1bdc918d3cc5a70ea7a914a11a2
Ben Y <byue2013@my.fit.edu>
2016-10-02 11:13:12 -0400

    [300. Longest Increasing Subsequence][Accepted]

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,38 +1,36 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        if(nums==null)
-            return 0;
-        if(nums.length<2)
-            return nums.length;
-        //10, 9, 2, 5
         /*
-        lowestEver = 0;
-        highestEver = 1;
-        low = 0;
-        high = 1;
+        longest increasing subsequence. idea is that the longest possible lis is the same size as the length of the list. so our dp array is initialized as the size of the nums.length ine each index we store the minimum number needed to extend a list to that length. Then we do a bs between the left and right walls to find the lowest position we can put this if it is equal to the right wall then expand the right wall boundary. you find the rightmost element where i am not greater than the next thing
         */
-        //2, 5, 0, 0, 0
-        int[] longestWithIndexLength = new int[nums.length+1];
-        //create array where each index represents the lowest value for that index
-        //for each number in the array, do a bs to find the first index where it's less than the number stored there
-        int lowestEver = 0;
-        int highestEver = 0;
+        
+        if(nums==null||nums.length==0)
+            return 0;
+        
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        int left = 0;
+        int right = 0;
         for(int i=0;i<nums.length;i++){
-            int low = lowestEver;
-            int high = highestEver;
             int cur = nums[i];
-            while(low<high){
-                int mid = (low+high)/2;
-                if(cur>longestWithIndexLength[mid])
-                    low = mid+1;
-                else
-                    high = mid;
-            }
-            
-            longestWithIndexLength[high] = cur;
-            if(high==highestEver)
-                highestEver++;
+            int pos = bs(dp, left, right, cur);
+            dp[pos] = cur;
+            if(pos==right)
+                right++;
         }
-        return highestEver;
+        return right;
     }
+    public int bs(int[] dp, int left, int right, int cur){
+        while(left<right){
+            int mid = (left+right)/2;
+            int midVal = dp[mid];
+            if(cur>midVal)
+                left = mid+1;
+            else
+                right = mid;
+        }
+        return right;
+    }
+    
 }
