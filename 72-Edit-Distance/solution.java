3b400ccecd219eecb0438336d391deb66ac55cca
Ben Y <byue2013@my.fit.edu>
2016-10-02 18:08:24 -0400

    [72. Edit Distance][Accepted]

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -1,31 +1,34 @@
 public class Solution {
-    /*
-    Idea is at each end point ask if same if so then it's free and just recurse.
-    Else if different delete from a or delete from b or replace and -1 index for both
-    */
     public int minDistance(String word1, String word2) {
-        if(word1.equals(word2))
+        /*
+        dp question: we create a 2d int array with rows of word1 length and cols of word2 length  then we look at the end of both words: if they're the same then i can get rid of both for free so just take i-1,j-1 else i have to try to get rid of each i-1,j and i, j-1. and return the min. top down
+        */
+        
+        if(word1==null||word2==null)
             return 0;
-        dp = new int[word1.length()][word2.length()];
-        return minDistance(word1, word1.length()-1, word2, word2.length()-1);
+        
+        int[][] dp = new int[word1.length()][word2.length()];
+        return minEdit(word1, word1.length()-1, word2, word2.length()-1, dp);
     }
-    int[][] dp;
-    public int minDistance(String a, int aIndex, String b, int bIndex){
-        if(aIndex==-1&&bIndex==-1)
-            return 0;
-        if(aIndex==-1||bIndex==-1)
-            return aIndex==-1?bIndex+1:aIndex+1;
+    
+    
+    public int minEdit(String a, int aIndex, String b, int bIndex, int[][] dp){
+        if(aIndex==-1)
+            return bIndex+1;
+        if(bIndex==-1)
+            return aIndex+1;
         
         if(dp[aIndex][bIndex]>0)
             return dp[aIndex][bIndex]-1;
-        int min;
-        if(a.charAt(aIndex)==b.charAt(bIndex))
-            min = minDistance(a, aIndex-1, b, bIndex-1);
-        else{
-            min = Math.min(minDistance(a, aIndex-1, b, bIndex-1), 
-                Math.min(minDistance(a, aIndex-1, b, bIndex),minDistance(a, aIndex, b, bIndex-1)))+1;
+        
+        int res;
+        if(a.charAt(aIndex)==b.charAt(bIndex)){
+            res = minEdit(a, aIndex-1, b, bIndex-1, dp);
+        }else{
+            res = Math.min(minEdit(a, aIndex, b, bIndex-1, dp),
+            Math.min(minEdit(a, aIndex-1, b, bIndex, dp), minEdit(a, aIndex-1, b, bIndex-1, dp)))+1;
         }
-        dp[aIndex][bIndex] = min+1;
-        return min;
+        dp[aIndex][bIndex] = res+1;
+        return res;
     }
 }
