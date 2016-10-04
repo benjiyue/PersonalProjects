7dbc35e44e5c0dbcdaac32cac22b1485c4e2f125
Ben Y <byue2013@my.fit.edu>
2016-10-03 22:20:30 -0400

    [128. Longest Consecutive Sequence][Accepted]

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -1,36 +1,45 @@
 public class Solution {
 	public int longestConsecutive(int[] nums) {
-		UF uf = new UF(nums.length);
-
-		//map from value to the index to avoid dups
 		Map<Integer, Integer> map = new HashMap();
 		for(int i=0;i<nums.length;i++){
 			if(map.containsKey(nums[i]))
 				continue;
-
-			map.put(nums[i],i);
-			if(map.containsKey(nums[i]+1))
-				uf.union(i, map.get(nums[i]+1));
-			if(map.containsKey(nums[i]-1))
-				uf.union(i, map.get(nums[i]-1));
+			map.put(nums[i], i);
 		}
-		Map<Integer, Integer> count = new HashMap();
-		int maxGroup = 1;
-		for(int i=0;i<uf.size.length;i++)
-			maxGroup = Math.max(maxGroup, uf.size[i]);
-		return maxGroup;
+		//we iterate over the values and try to union the nums[i] with nums[i]-1 and nums[i]+1 if the -1 and +1 exist in the map
+
+		UF uf = new UF(nums.length);
+
+		for(int i=0;i<nums.length;i++){
+			int val = nums[i];
+			if(map.containsKey(val-1)){
+				uf.union(map.get(val), map.get(val-1));
+			}
+			if(map.containsKey(val+1)){
+				uf.union(map.get(val), map.get(val+1));
+			}
+		}
+
+		//we count the number of indices that are mapped to some representative and the largest value will be the result
+		int maxSize = 1;
+		for(int i=0;i<uf.sizes.length;i++){
+			maxSize = Math.max(uf.sizes[i], maxSize);
+		}
+		return maxSize;
 	}
 
+	public class UF{
 
-	class UF{
-		int[] size;
-		int[] representative;
-		public UF(int length){
-			size = new int[length];
-			Arrays.fill(size, 1);
-			representative = new int[length];
-			for(int i=0;i<representative.length;i++)
-				representative[i] = i;
+		int[] rep;
+		int[] sizes;
+
+		public UF(int size){
+			rep = new int[size+1];
+			for(int i=0;i<size+1;i++)
+				rep[i] = i;
+
+			sizes = new int[size+1];
+			Arrays.fill(sizes, 1);
 		}
 
 		public void union(int a, int b){
@@ -39,21 +48,25 @@ public class Solution {
 			if(aRep==bRep)
 				return;
 
-			if(size[aRep]>size[bRep]){
-				size[aRep]+=size[bRep];
-				representative[bRep] = aRep;
+			if(sizes[aRep]>sizes[bRep]){
+				rep[bRep] = aRep;
+				sizes[aRep]+=sizes[bRep];
 			}else{
-				size[bRep]+=size[aRep];
-				representative[aRep] = bRep;
+				rep[aRep] = bRep;
+				sizes[bRep]+=sizes[aRep];
 			}
+
 		}
 
-		public int find(int node){
-			while(representative[node]!=node){
-				representative[node] = representative[representative[node]];
-				node = representative[node];
+		public int find(int index){
+			//while i am not the rep of myself set my rep to my grandparent then continue the while
+			while(rep[index]!=index){
+				//set my rep to my grandparent
+				rep[index] = rep[rep[index]];
+				//set the cur index to grandparent
+				index = rep[index];
 			}
-			return node;
+			return index;
 		}
 
 	}
