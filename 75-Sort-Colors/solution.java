dad9c224dc895b96a2c7f4d2b836076695f5edf0
Ben Y <byue2013@my.fit.edu>
2016-10-06 10:27:05 -0400

    [75. Sort Colors][Accepted]

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -2,6 +2,12 @@ public class Solution {
     public void sortColors(int[] nums) {
         /*
             idea is to havea a left pointer pointing to the next position to put the 0, an iterator index and a right index pointing to the next position to put the 2, not that when we move the 0 to the left pointer we increment both the iterator and the left because we will now have a 0 in the left pointer and a 1 in the iterator position but the next position to put the next 0 will be left+1 and also since the iterator skips 1's we can skip it too sonce for sure it's a 1. But when swapping the 2 we dont actually know what we get it could be a 0,1, or 2 so we dont move the iterator only the right position since it now holds a 2 and the next position to put the 2 is right-1
+            
+            You though that you could just put a 0 to the left and a 1 to the right when the nums[iter]==0 but again what if the array is all 0's 
+             i
+             l
+            [0,0,0,0,0,0]
+            
         */
         
         if(nums==null||nums.length<2)
@@ -12,9 +18,8 @@ public class Solution {
         int right = nums.length-1;
         while(iter<=right){
             if(nums[iter]==0){
-                int temp = nums[left];
-                nums[left] = nums[iter];
-                nums[iter] = temp;
+                nums[iter] = 1;
+                nums[left] = 0;
                 left++;iter++;
             }else if(nums[iter]==2){
                 nums[iter] = nums[right];
