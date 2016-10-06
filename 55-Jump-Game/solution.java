da155dd71b2f5ba2624a9db9ce81a0083732eae8
Ben Y <byue2013@my.fit.edu>
2016-10-05 22:14:44 -0400

    [55. Jump Game][Accepted]

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,14 +1,22 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        int farthest = 0;
-        for(int i=0;i<nums.length;i++){
-            if(farthest>=nums.length-1)
+        /*
+        we keep track of the farthest index we can get to based on the things we've seen then we keep going one by one while the index is less than the maxPos, it we reach maxPos and are not able to go any more then return false by breaking else if any jump can lead me to go beyond the last index return true
+        
+        max = 3
+        3
+        
+        */
+        if(nums==null||nums.length==0)
+            return true;
+            
+        int maxPos = nums[0];
+        for(int i=1;i<=maxPos&&i<nums.length;i++){
+            maxPos = Math.max(maxPos, i+nums[i]);
+            if(maxPos>=nums.length-1)
                 return true;
-            if(farthest>=i){
-                farthest = Math.max(farthest,i+nums[i]);
-            }else
-                return false;
         }
-        return true;
+        return maxPos>=nums.length-1;
+        
     }
 }
