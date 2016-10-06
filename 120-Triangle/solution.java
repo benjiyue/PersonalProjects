2d7c80df8f8a56e5d3eaeab53cc4639909e84d1b
Ben Y <byue2013@my.fit.edu>
2016-10-05 20:46:59 -0400

    [120. Triangle][Accepted]

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -3,32 +3,18 @@ public class Solution {
         /*
         we do a dfs with memoization. we set our anchor and then recurse to the nxt legvel down and there we can schoose i or i+1, then explore both. when we return we save the best choice for this depth this idnex and memoize before we return
         */
-        
-        return dfs(triangle, 0, 0, new HashMap<String, Integer>());
-        /*
-[
-    8[2],9
-    [3,4],
-   [6,5,7]
-]
-        */
-    }
-    
-    public int dfs(List<List<Integer>> triangle, int depth, int index, Map<String, Integer> map){
-        if(depth>=triangle.size())
-            return 0;
-        
-        String key = depth+","+index;
-        if(map.containsKey(key))
-            return map.get(key);
-            
-        int left = dfs(triangle, depth+1, index, map);
-        int right = dfs(triangle, depth+1, index+1, map);
-        int min = Math.min(left, right);
-        int val = triangle.get(depth).get(index);
-        map.put(key, min+val);
-        
-        return min+val;
-        
+        int[][] dp = new int[triangle.size()][triangle.size()+1];
+        //fill up the last level
+        for(int i=0;i<triangle.get(triangle.size()-1).size();i++){
+            dp[triangle.size()-1][i] = triangle.get(triangle.size()-1).get(i);
+        }
+        //iterate for ecah row backwards
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j] = triangle.get(i).get(j)+Math.min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+
     }
 }
