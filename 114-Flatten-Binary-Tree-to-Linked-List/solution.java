a1628b24a5392db8ae5fbb7970110246dd46c92c
Ben Y <byue2013@my.fit.edu>
2016-10-08 10:14:14 -0400

    [114. Flatten Binary Tree to Linked List][Accepted]

--- 114-Flatten-Binary-Tree-to-Linked-List/solution.java
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution.java
@@ -8,31 +8,19 @@
  * }
  */
 public class Solution {
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
-        //we flatten by making the .right equal to the .left, but we have to first save the .right then insert the .left if it's non null then go all the way to the right then put the last thing to the saved next
-        
+        /*
+        .right of each node is equal to the next preorder traversal node
+        */
         if(root==null)
             return;
         
-        postOrder(root);
-    }
-    
-    public TreeNode postOrder(TreeNode root){
-        if(root==null)
-            return null;
-        
-        TreeNode left = postOrder(root.left);
-        TreeNode right = postOrder(root.right);
-        
-        if(left!=null){
-            root.left = null;
-            TreeNode next = root.right;
-            root.right = left;
-            while(left.right!=null)
-                left = left.right;
-            left.right = next;
-        }
-        return root;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
         
     }
 }
